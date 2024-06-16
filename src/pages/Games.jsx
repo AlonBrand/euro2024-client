799d3fa8fd9d42170d54ed8e82b6e0c15f6872a7
Etamar <etamar1122@gmail.com>
2024-06-16 18:31:12 +0300

    etamars

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -4,6 +4,7 @@ import { GameTab } from '../components/GameTab';
 import { games} from '../constants/games';
 import euroLogo from "../images/euro-logo.svg"
 import { Pagination } from '@mui/material';
+import { SiComsol } from 'react-icons/si';
 
 const itemsPerPage = 5;
 
@@ -11,7 +12,7 @@ function Games(prop) {
   const {setModalContent, setOpen} = prop;
   const [reFetch, setReFetch] = useState('');
   // const [userBets, setUserBets] = useStat e();
-  const [showGroupGames, setShowGroupGames] = useState(true);
+  const [showAllGames, setshowAllGames] = useState(true);
   // const [showEighthGames, setShowEightGames] = useState(false);
   // const [showQuarterGames, setShowQuarterGames] = useState(false);
   // const [showSemiGames, setShowSemiGames] = useState(false);
@@ -31,44 +32,48 @@ function Games(prop) {
     }
   };
 
-  const paginatedData = games?.slice((page - 1) * itemsPerPage, page * itemsPerPage);
 
+  const paginatedData = games?.filter(game => (showAllGames ? true : game.date > new Date()  )).slice((page - 1) * itemsPerPage, page * itemsPerPage);
+  console.log("paginatedData", paginatedData)
   useEffect(() => {
-      const getUserBets = () => {
-          try{
-                const tempBets = [];
-                const tempGames = [];
-                fetch(`${apiUrl}/userBets/${window.USER_ID}`)
-                .then((response) => response.json()
-                .then((data) => {
-                  // const sortedData = data?.userBets?.sort((a, b)=>a[2] - b[2]);
-                    for(let bet of data?.userBets) {
-                      if(Array.isArray(bet) && bet?.length >= 4) {
-                        const object = Object.assign({id: bet[2], value: `Current bet: ${bet[3]} - ${bet[4]}`, scoreA: bet[3], scoreB: bet[4]})
-                        tempBets.push(object);
+        const getUserBets = () => {
+            try{
+                    const tempBets = [];
+                    const tempGames = [];
+                    fetch(`${apiUrl}/userBets/${window.USER_ID}`)
+                    .then((response) => response.json()
+                    .then((data) => {
+                    // const sortedData = data?.userBets?.sort((a, b)=>a[2] - b[2]);
+                        for(let bet of data?.userBets) {
+                        if(Array.isArray(bet) && bet?.length >= 4) {
+                            const object = Object.assign({id: bet[2], value: `Current bet: ${bet[3]} - ${bet[4]}`, scoreA: bet[3], scoreB: bet[4]})
+                            tempBets.push(object);
+                        }
                     }
-                  }
 
-                  for(let game of data?.games) {
-                    if(Array.isArray(game) && game?.length >= 4) {
-                      tempGames.push({
-                        id: game[0],
-                        scoreA: game[3],
-                        scoreB: game[4],
-                      })
+                    for(let game of data?.games) {
+                        if(Array.isArray(game) && game?.length >= 4) {
+                        tempGames.push({
+                            id: game[0],
+                            scoreA: game[3],
+                            scoreB: game[4],
+                        })
+                        }
                     }
-                  }
-                  setBets(tempBets);
-                  setRealGames(tempGames);
-              }))
-          } catch(e) {
-              console.log(e)
-          }
-      }
-      getUserBets();
+                    setBets(tempBets);
+                    setRealGames(tempGames);
+                }))
+            } catch(e) {
+                console.log(e)
+            }
+        }
+        getUserBets();
   }, [reFetch, apiUrl]);
 
-  const toggleShowGroupMatches = () => setShowGroupGames((prevShow)=>!prevShow)
+  const toggleShowGroupMatches = () => { 
+        setshowAllGames((prevShow)=>!prevShow) 
+        setPage(1);
+  }
   // const toggleShowEighthFinal = () => setShowEightGames((prevShow)=>!prevShow)
   // const toggleShowQuarterFinal = () => setShowQuarterGames((prevShow)=>!prevShow);
   // const toggleShowSemiFinal = () => setShowSemiGames((prevShow)=>!prevShow);
@@ -80,9 +85,11 @@ function Games(prop) {
       <>
         {
           Object.values(paginatedData)?.map((game, index) => {
+            console.log(paginatedData)
             // const curr_date = new Date();
             // const diffTime = curr_date - game?.date;
-            if(game?.status === "Groups" && !showGroupGames) return null;
+            
+            // if(game?.status === "Groups" && !showGroupGames && (new Date() < game.date)) return null;
             // if(game.status === "Eighth" && !showEighthGames) return;
             // if(game.status === "Quarter" && !showQuarterGames) return;
             // if(game.status === "Semi" && !showSemiGames) return;
@@ -120,12 +127,9 @@ function Games(prop) {
       <h2 className='pageTitle' style={{ padding: "20px" }}>Matches</h2>
       <div className='games' id="targetElement">
         <div className="game-tab-container" style={{ marginBottom: "30px", padding: "10px", fontWeight: "bold" }} onClick={toggleShowGroupMatches}>
-          {showGroupGames ? 'Hide Group Matches' : 'Reveal Group Matches'}
+          {showAllGames ? 'Show Available Matches' : 'Show All Matches'}
         </div>
-        {
-          showGroupGames &&
-          getGamesContent(games)
-        }
+          {getGamesContent(games)}
         {/* <div className="game-tab-container" style={{ marginBottom: "30px", padding: "10px", fontWeight: "bold" }} onClick={toggleShowEighthFinal}>
           {showEighthGames ? 'Hide' : 'Reveal'}
         </div> */}
@@ -142,15 +146,12 @@ function Games(prop) {
           {showFinalGames ? 'Hide Final' : 'Reveal Final'}
         </div>
         {getGamesContent(finalGames)} */}
-        {
-          showGroupGames &&
           <Pagination
             count={Math.ceil(games.length / itemsPerPage)}
             page={page}
             onChange={handleChange}
             sx={{ margin: "1vh 0 2vh 0", display: 'flex', justifyContent: 'center', fontSize: '1.2rem' }}
           />
-        }
       </div>
     </>
   )
